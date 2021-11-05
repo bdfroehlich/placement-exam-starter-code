f67e7deb60ce49f51bf0529768dac32435acaeb0
Benjamin Froehlich <bdfroehlich0@gmail.com>
2021-11-04 20:54:49 -0500

    commiting all

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,97 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+let yourName = "Ben Froehlich" // HINT: Replace this with your own name!
+document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
 
-// Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+//function to be executed when events for GB are executed to update GB count and return current total GB count to quantity-GB using #qty-gb id selector and .innerHTML
+function updateGb(totalGb){
+    let gb = document.querySelector('#qty-gb')
+    gb.innerHTML = totalGb
+}
+
+
+document.querySelector('#add-gb').addEventListener('click', function() {
+        gb++   
+    updateGb(gb)
+    runningTotal() 
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.querySelector('#minus-gb').addEventListener('click', function() {
+    if (gb > 0) {
+        gb--   
+    }
+    updateGb(gb)
+    runningTotal() 
+})
+
+//function to be executed when events for CC are executed to update CC count and return current total CC count to quantity-CC using #qty-cc id selector and .innerHTML
+function updateCc(totalCc){
+    let cc = document.querySelector('#qty-cc')
+    cc.innerHTML = totalCc
+}
+
+document.querySelector('#add-cc').addEventListener('click', function() {
+    cc++   
+    updateCc(cc)
+    runningTotal() 
+})
+
+document.querySelector('#minus-cc').addEventListener('click', function() {
+    if (cc > 0) {
+        cc--   
+    }
+    updateCc(cc)
+    runningTotal() 
+})
+
+//function to be executed when events for sugar are executed to update sugar count and return current total sugar count to quantity-sugar using #qty-sugar id selector and .innerHTML
+function updateSugar(totalSugar){
+    let sugar = document.querySelector('#qty-sugar')
+    sugar.innerHTML = totalSugar
+}
+
+document.querySelector('#add-sugar').addEventListener('click', function() {
+    sugar++
+    updateSugar(sugar)
+    runningTotal()    
+})
+
+document.querySelector('#minus-sugar').addEventListener('click', function() {
+    if (sugar > 0) {
+        sugar--   
+    }
+    updateSugar(sugar)
+    runningTotal() 
+})
+
+function runningTotal() {
+    var total = 0
+    var quantity = document.querySelectorAll('#qty-gb,#qty-cc,#qty-sugar')
+    for (var i = 0; i<quantity.length; i++){
+        total += parseInt(quantity[i].innerHTML)
+    }
+    document.querySelector('#qty-total').innerHTML = total
+}
+
+
+
+
+/*let totalArray = document.querySelectorAll('#qty-gb, #qty-cc, #qty-sugar')
+console.log(totalArray)
+
+totalArray.forEach(cookie => {
+    total = total + cookie.id
+})
+*/
+
+
+/*function updateTotal()
+{
+    for (i<0; i<totalArray.length;i++)
+    total = total + totalArray[i]
+}
+*/
